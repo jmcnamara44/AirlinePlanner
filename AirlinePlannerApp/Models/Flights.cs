139198070744a612050736813235c23b080b09c3
Jimmy McNamara and Justin Roller <jimmy@>
2018-05-07 16:06:00 -0700

    add delayed method.

--- AirlinePlannerApp/Models/Flights.cs
+++ AirlinePlannerApp/Models/Flights.cs
@@ -10,11 +10,13 @@ namespace AirlinePlannerApp.Models
   {
     private int _id;
     private string _departureTime;
+    private int _isDelayed;
 
-    public Flights(string departureTime, int id = 0)
+    public Flights(string departureTime, int isDelayed = 0, int id = 0)
     {
       _id = id;
       _departureTime = departureTime;
+      _isDelayed = isDelayed;
     }
 
     public static List<Flights> GetAll()
@@ -29,7 +31,8 @@ namespace AirlinePlannerApp.Models
         {
           int id = rdr.GetInt32(0);
           string departureTime = rdr.GetDateTime(1).ToString();
-          Flights newFlights = new Flights(departureTime, id);
+          int isDelayed = rdr.GetInt32(2);
+          Flights newFlights = new Flights(departureTime, isDelayed, id);
           allFlights.Add(newFlights);
         }
         conn.Close();
@@ -72,6 +75,36 @@ namespace AirlinePlannerApp.Models
         }
         return cities;
       }
-      public List<
+      public List<Cities> GetIsDelayed()
+      {
+          MySqlConnection conn = DB.Connection();
+          conn.Open();
+          MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+          cmd.CommandText = @"SELECT cities.* FROM flights
+              JOIN cities_flights ON (flights.id = cities_flights.flight_id)
+              JOIN cities ON (cities_flights.departure_city_id = cities.id)
+              WHERE flights.is_delayed = @IsDelayed;";
+
+          MySqlParameter flightIdParameter = new MySqlParameter();
+          flightIdParameter.ParameterName = "@IsDelayed";
+          flightIdParameter.Value = _isDelayed;
+          cmd.Parameters.Add(flightIdParameter);
+
+          MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+          List<Cities> cities = new List<Cities>{};
+
+          while(rdr.Read())
+          {
+            int cityId = rdr.GetInt32(0);
+            string cityName = rdr.GetString(1);
+            Cities newCity = new Cities(cityName, cityId);
+            cities.Add(newCity);
+          }
+          conn.Close();
+          if (conn != null)
+          {
+              conn.Dispose();
+          }
+          return cities;
     }
   }
